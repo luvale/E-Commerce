6a9e2697c6676f3e9484bd7972eee9c320590768
luvale <valeriae200@hotmail.com>
2021-11-22 18:26:34 -0500

    create views

--- E-Commerce/src/app/app.module.ts
+++ E-Commerce/src/app/app.module.ts
@@ -4,9 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// Views
+import { LoginComponent } from './views/login/login.component';
+import { HomeComponent } from './views/home/home.component';
+import { ShopComponent } from './views/shop/shop.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    ShopComponent
   ],
   imports: [
     BrowserModule,
