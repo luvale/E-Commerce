c70215ac460a40942431078c4b85bad15f226919
luvale <valeriae200@hotmail.com>
2021-11-23 11:54:20 -0500

    get all products

--- E-Commerce/src/app/services/products.service.ts
+++ E-Commerce/src/app/services/products.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams} from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductsService {
+  private Url = "https://fakestoreapi.com/products"; 
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  getAllProducts(){
+    return this.http.get(this.Url)
+  }
 }
--- E-Commerce/src/app/views/home/home.component.ts
+++ E-Commerce/src/app/views/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductsService } from '../../services/products.service'
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor( private products:ProductsService ) { }
+
+  allProducts:Object = {}
 
   ngOnInit(): void {
+    this.getAllProducts()
+  }
+  
+  getAllProducts(){
+    this.products.getAllProducts().subscribe(data => {
+      console.log(data);
+      this.allProducts = data;
+    })
   }
 
 }
