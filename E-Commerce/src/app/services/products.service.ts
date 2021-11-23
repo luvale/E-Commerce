cd3e0f1f9174dafbe9ce1642248bf85a291bcc71
luvale <valeriae200@hotmail.com>
2021-11-23 12:48:20 -0500

    deleted what i did not use in products service

--- E-Commerce/src/app/services/products.service.ts
+++ E-Commerce/src/app/services/products.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpParams} from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
