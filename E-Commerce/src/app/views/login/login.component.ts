726a59b424df9a61579efdbd14fedcd01bf07c57
luvale <valeriae200@hotmail.com>
2021-11-22 20:40:23 -0500

    capture LoginForm.value

--- E-Commerce/src/app/views/login/login.component.ts
+++ E-Commerce/src/app/views/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormControlName } from '@angular/forms'
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  LoginForm = new FormGroup({
+    email: new FormControl('', Validators.required),
+    password: new FormControl('', Validators.required)
+  })
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  Login(form:Object){
+    console.log(form)
   }
 
 }
